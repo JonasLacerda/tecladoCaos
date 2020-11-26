e6b55569c4c1c2d6ca524032b5679ad57b169755
JonasLacerda <65193517+JonasLacerda@users.noreply.github.com>
2020-11-26 06:36:34 -0300

    Update teclado.ino

--- teclado.ino
+++ teclado.ino
@@ -1,25 +1,29 @@
 #include <Keypad.h>
 #include <Keyboard.h>
 
+#include "U8glib.h"
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
 const byte ROWS = 10; //four rows
 const byte COLS = 5; //three columns
 
 char keys[ROWS][COLS] = {
 {10,':',';','p',8},
-{6,'.','l','o',0},
-{10,',','k','i',14},
-{15,'m','j','u',19},
-{32,'n','h','y',24},
-{32,'b','g','t',29},
-{33,'v','f','r',34},
-{35,'c','d','e',27},
-{40,'x','s','w',44},
-{32,'z','a','q',49}
+{6,'.','l','o',99},
+{11,12,'k','i',14},
+{16,'m','j','u',19},
+{' ','n','h','y',24},
+{' ','b','g','t',29},
+{31,'v','f','r',35},
+{36,37,'d','e',27},
+{33,'x','s','w',44},
+{46,'z','a','q',49}
 };
 
 byte rowPins[ROWS] = {A3, A2, A0, A1, 14, 8, 6, 4, 7, 5}; //connect to the row pinouts of the kpd
-byte colPins[COLS] = {9, 1, 10, 16, 15}; //connect to the column pinouts of the kpd
-
+byte colPins[COLS] = {9, 1, 10, 16, 15}; //connect to the column pinouts of the kaaq
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 unsigned long loopCount;
@@ -34,6 +38,14 @@ void setup() {
     msg = "";
 
     Keyboard.begin();
+
+    u8g.setColorIndex(3);
+    u8g.setFont(u8g_font_unifont);    
+    pinMode(8, OUTPUT);
+    u8g.firstPage();  
+    do {
+      u8g.drawStr( 0, 22, "Teclado do Caos");
+    } while( u8g.nextPage() );
 }
 
 
@@ -50,9 +62,9 @@ void loop() {
     // Returns true if there are ANY active keys.
     if (kpd.getKeys())
     {
-        for (int i=0; i<LIST_MAX; i++)   // Scan the whole key list.
+      for (int i=0; i<LIST_MAX; i++)   // Scan the whole key list.      
         {
-            if ( kpd.key[i].stateChanged )   // Only find keys that have changed state.
+          if ( kpd.key[i].stateChanged )   // Only find keys that have changed state.
             {
                 switch (kpd.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
                     case PRESSED:
@@ -71,15 +83,143 @@ void loop() {
                 Serial.print(kpd.key[i].kchar);                
                 Serial.println(msg);
                 if(msg == " HOLD."){
-                  Keyboard.press(kpd.key[i].kchar);
+                  if(kpd.key[i].kchar == 31){
+                    Keyboard.press(0x85);
+                    u8g.firstPage(); 
+                    do {
+                      u8g.drawStr( 0, 22, "Teclado do Caos");
+                      u8g.drawStr( 0, 33, "SHIFT");
+                    } while( u8g.nextPage() );
+                  }
+                  else if(kpd.key[i].kchar == 33){
+                    Keyboard.press(0x86);
+                    u8g.firstPage(); 
+                    do {
+                      u8g.drawStr( 0, 22, "Teclado do Caos");
+                      u8g.drawStr( 0, 33, "ALT");
+                    } while( u8g.nextPage() );
+                  }
+                  else if(kpd.key[i].kchar == 16){
+                    Keyboard.press(0xD8);
+                  }
+                  else if(kpd.key[i].kchar == 12){
+                    Keyboard.press(0xDA);
+                  }
+                  else if(kpd.key[i].kchar == 11){
+                    Keyboard.press(0xD9);
+                  }
+                  else if(kpd.key[i].kchar == 6){
+                    Keyboard.press(0xD7);
+                  }
+                  else if(kpd.key[i].kchar == 46){
+                    Keyboard.press(0x84); 
+                    u8g.firstPage(); 
+                    do {
+                      u8g.drawStr( 0, 22, "Teclado do Caos");
+                      u8g.drawStr( 0, 33, "CTRL");
+                    } while( u8g.nextPage() );  
+                  }
+                  else{
+                     Keyboard.press(kpd.key[i].kchar);
+                  }  
                 }
                 else if(msg == " PRESSED."){
-                  Keyboard.press(kpd.key[i].kchar);
+                  if(kpd.key[i].kchar == 27){
+                    Keyboard.write(0x86);
+                  }
+                   else if(kpd.key[i].kchar == 33){
+                    Keyboard.press(0x86);
+                    u8g.firstPage(); 
+                    do {
+                      u8g.drawStr( 0, 22, "Teclado do Caos");
+                      u8g.drawStr( 0, 33, "ALT");
+                    } while( u8g.nextPage() );
+                  }
+                  else if(kpd.key[i].kchar == 46){
+                    Keyboard.press(0x84); 
+                    u8g.firstPage(); 
+                    do {
+                      u8g.drawStr( 0, 22, "Teclado do Caos");
+                      u8g.drawStr( 0, 33, "CTRL");
+                    } while( u8g.nextPage() );  
+                  }
+                  else if(kpd.key[i].kchar == 16){
+                    Keyboard.press(0xD8);
+                  }
+                  else if(kpd.key[i].kchar == 35){
+                    Keyboard.press(0xB3);
+                  }
+                  else if(kpd.key[i].kchar == 12){
+                    Keyboard.press(0xDA);
+                  }
+                  else if(kpd.key[i].kchar == 11){
+                    Keyboard.press(0xD9);
+                  }
+                  else if(kpd.key[i].kchar == 6){
+                    Keyboard.press(0xD7);
+                  }
+                  else if(kpd.key[i].kchar == 49){
+                    Keyboard.write(0xB1);
+                  }
+                  else if(kpd.key[i].kchar == 37){
+                    Keyboard.write('c');
+                  }
+                  else if(kpd.key[i].kchar == 31){
+                    Keyboard.press(0x85);
+                    u8g.firstPage(); 
+                    do {
+                      u8g.drawStr( 0, 22, "Teclado do Caos");
+                      u8g.drawStr( 0, 33, "SHIFT");
+                    } while( u8g.nextPage() ); 
+                  }                   
+                  else{
+                    Keyboard.press(kpd.key[i].kchar);
+                  }                  
                 }
-                else if(msg == " RELEASED."){
-                  Keyboard.release(kpd.key[i].kchar);
+                
+                else if(msg == " RELEASED." || msg == " IDLE."){
+                  if(kpd.key[i].kchar == 46){
+                    Keyboard.release(0x84);
+                    u8g.firstPage();  
+                    do {
+                      u8g.drawStr( 0, 22, "Teclado do Caos");
+                    } while( u8g.nextPage() );
+                  }
+                  else if(kpd.key[i].kchar == 33){
+                    Keyboard.release(0x86);
+                    u8g.firstPage();  
+                    do {
+                      u8g.drawStr( 0, 22, "Teclado do Caos");
+                    } while( u8g.nextPage() ); 
+                  }
+                  else if(kpd.key[i].kchar == 31){
+                    Keyboard.release(0x85);
+                    u8g.firstPage();  
+                    do {
+                      u8g.drawStr( 0, 22, "Teclado do Caos");
+                    } while( u8g.nextPage() );
+                  }
+                  else if(kpd.key[i].kchar == 16){
+                    Keyboard.release(0xD8);
+                  }
+                  else if(kpd.key[i].kchar == 35){
+                    Keyboard.release(0xB3);
+                  }
+                  else if(kpd.key[i].kchar == 12){
+                    Keyboard.release(0xDA);
+                  }
+                  else if(kpd.key[i].kchar == 11){
+                    Keyboard.release(0xD9);
+                  }
+                  else if(kpd.key[i].kchar == 6){
+                    Keyboard.release(0xD7);
+                  }                  
+                  else{
+                    Keyboard.release(kpd.key[i].kchar);
+                  }
+                  
                 }
             }
         }
-    }
-}  // End loop  que merda e essa 
+    }   
+}
